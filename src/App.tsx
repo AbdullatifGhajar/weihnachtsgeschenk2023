29f166cc267e94568583f124ac5bd84d67069094
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-12-08 23:31:10 +0100

    Use routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import QrCode from './QrCode';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <Routes>  
+        <Route path="/" element={<QrCode />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
